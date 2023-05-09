92160adf653e853c91c1089ee45c647521e2490f
Saul <saul.crespo.sc@gmail.com>
2023-05-09 09:52:28 +0200

    Tie

--- eclipse-workspace/MiHangar/src/miHangar/Tie.java
+++ eclipse-workspace/MiHangar/src/miHangar/Tie.java
@@ -1,5 +1,25 @@
 package miHangar;
 
-public class Tie {
+public class Tie extends Nave {
+	private float cilindrada;
 
+	
+	public Tie() {
+		super();
+		this.cilindrada = 0.0f;
+	}
+	
+	public Tie(String matricula, String flota, float cilindrada) {
+		super(matricula, flota);
+		this.cilindrada = cilindrada;
+	}
+	
+	
+	public float getCilindrada() {
+		return cilindrada;
+	}
+
+	public void setCilindrada(float cilindrada) {
+		this.cilindrada = cilindrada;
+	}
 }
