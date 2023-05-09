500332d1d9c110bf701ec65d26be6e7ac23c9c0e
Saul <saul.crespo.sc@gmail.com>
2023-05-09 09:46:13 +0200

    inicial

---
+++ eclipse-workspace/MiHangar/src/miHangar/Nave.java
@@ -0,0 +1,56 @@
+package miHangar;
+
+public abstract class Nave {
+	protected String matricula;
+	protected Flota flota;
+	
+	private enum Flota  {
+		IMPERIO(0.5),
+		REPUBLICA(0.75);
+		
+		private double tasa;
+		
+		Flota(double tasa) {
+			this.setTasa(tasa);
+		}
+		public double getTasa() {
+			return tasa;
+		}
+		public void setTasa(double tasa) {
+			this.tasa = tasa;
+		}
+	}
+	
+	protected Nave() {
+		matricula = "0000AAA";
+		this.flota = Flota.IMPERIO;
+	}
+	
+	protected Nave(String matricula, String flota) {
+		this.matricula = matricula;
+	}
+	
+	public String getMatricula() {
+		return matricula;
+	}
+
+	public void setMatricula(String matricula) {
+		this.matricula = matricula;
+	}
+
+	public Flota getFlota() {
+		return flota;
+	}
+
+	public void setFlota(Flota flota) {
+		this.flota = flota;
+	}
+	
+	public void setFlota(String flota) {
+		this.flota = (flota.toLowerCase().charAt(0) == 'i')? Flota.IMPERIO: Flota.REPUBLICA;
+	}
+	
+	public double getTasa() {
+		return flota.getTasa();
+	}
+}
---
+++ eclipse-workspace/MiHangar/src/miHangar/Wing.java
@@ -0,0 +1,5 @@
+package miHangar;
+
+public class Wing extends Nave {
+
+}
